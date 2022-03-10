7123d27da52116000d71d8a6a4196b153a110f84
rubens.rangel <rubens.rangel@b2wdigital.com>
2022-03-10 14:57:34 -0300

    SECOND

--- src/main/java/com/test/cloud/SpringSpannerApp/SpringSpannerAppApplication.java
+++ src/main/java/com/test/cloud/SpringSpannerApp/SpringSpannerAppApplication.java
@@ -1,26 +1,9 @@
 package com.test.cloud.SpringSpannerApp;
 
 
-
-
-
-import com.google.cloud.spring.core.Credentials;
-import com.test.cloud.SpringSpannerApp.entities.Order;
-import com.test.cloud.SpringSpannerApp.repository.OrderRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.UUID;
 
 
 
@@ -33,41 +16,5 @@ public class SpringSpannerAppApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-	}
-}
-
-@RestController
-class OrderController {
-	private final OrderRepository orderRepository;
-
-	OrderController(OrderRepository orderRepository) {
-		this.orderRepository = orderRepository;
-	}
-
-	@GetMapping("/api/orders/{id}")
-	public Order getOrder(@PathVariable String id) {
-		return orderRepository.findById(id)
-				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, id + " not found"));
-	}
-
-	@PostMapping("/api/orders")
-	public String createOrder(@RequestBody Order order) {
-		// Spanner currently does not auto generate IDs
-		// Generate UUID on new orders
-		order.setId(UUID.randomUUID().toString());
-		order.setTimestamp(LocalDateTime.now());
-
-		order.getItems().forEach(item -> {
-			// Assign parent ID, and also generate child ID
-			item.setOrderId(order.getId());
-			item.setOrderItemId(UUID.randomUUID().toString());
-		});
-
-		Order saved = orderRepository.save(order);
-		return saved.getId();
-	}
-
-	public interface CredentialsProvider {
-		Credentials getCredentials() throws IOException;
 	}
 }
